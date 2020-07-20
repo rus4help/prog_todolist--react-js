b09ba0c1b41d094a2201f40cdca30cf7ea1ebc8a
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-07-20 11:13:21 +0300

    Структурировал приложение

---
+++ src/components/todo-list.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import TodoListItem from './todo-list-item'
+
+const TodoList = () => {
+
+    const items = ['Drink Coffee', 'Build Awesome App']
+    return (
+        <ul>
+            <li><TodoListItem /></li>
+            <li><TodoListItem /></li>
+        </ul>
+    )
+}
+
+export default TodoList
