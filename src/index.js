254055aa978c879296f2c914565483e4da28666f
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-07-21 13:10:06 +0300

    Доделал функционал

--- src/index.js
+++ src/index.js
@@ -7,11 +7,17 @@ import TodoList from './components/todo-list'
 
 const App = () => {
 
+    const todoData = [
+        { label: 'Drink Coffee', important: false, id: 1 },
+        { label: 'Make Awesome App', important: true, id: 2 },
+        { label: 'Have a lunch', important: false, id: 3 }
+    ]
+
     return (
         <div>
             <AppHeader />
             <SearchPanel />
-            <TodoList />
+            <TodoList todos={todoData} />
         </div>
     )
 }
