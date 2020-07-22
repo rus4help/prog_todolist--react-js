ca4a5dc9d1900b71cb17e5d0be8219d7b1d6c53d
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-07-22 12:01:10 +0300

    Изменяем состояние компонента

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,21 +4,37 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+  state = {
+    done: false
+  }
+
+  onLabelClick = () => {
+    this.setState({
+      done: true
+    })
+  }
+
   render() {
 
     const { label, important = false } = this.props;
+    const { done } = this.state;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
 
     const style = {
       color: important ? 'steelblue' : 'black',
       fontWeight: important ? 'bold' : 'normal'
     };
 
-
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
-          style={style}>
+          style={style}
+          onClick={this.onLabelClick}>
           {label}
         </span>
 
