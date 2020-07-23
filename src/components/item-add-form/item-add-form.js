cf84fb72cc8c55c4ec3de07cbd2ed00551e7e899
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-07-23 16:37:19 +0300

    Доделан функционал добавления нового элемента из формы в список дел

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,13 +3,32 @@ import React, { Component } from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+
+    state = {
+        label: ''
+    }
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        this.props.onItemAdded(this.state.label)
+        this.setState({
+            label: ''
+        })
+    }
+
     render() {
         return (
-            <div className="item-add-form">
+            <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+                <input type="text" value={this.state.label} className="form-control" onChange={this.onLabelChange} placeholder="What needs to be done?" />
                 <button
-                    className="btn btn-outline-secondary"
-                    onClick={() => this.props.onItemAdded('Hello')}>Add Item</button>
-            </div>
+                    className="btn btn-outline-secondary">Add</button>
+            </form >
         )
     }
 }
